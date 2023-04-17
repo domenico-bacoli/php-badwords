5107f62bb4cd984a7c4d8967b8c94a0ab558532d
domenicobacoli <domenico.bacoli89@gmail.com>
2023-04-17 17:26:14 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    <h1>Compila i campi</h1>
+    <?php 
+    
+    ?>
+
+    <form action="risultato.php" method="GET">
+        <textarea name="user-text" cols="30" rows="10" required></textarea>
+        <br>
+        <input type="text" name="badwords" placeholder="Inserisci bad word" required>
+        <br>
+        <input type="submit">
+    </form>
+
+</body>
+</html>
---
+++ risultato.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badword Risultato</title>
+</head>
+<body>
+    <?php 
+        $text = $_GET['user-text'];
+        $badWords = $_GET['badwords'];
+        $replace = '***';
+        $censuredText = str_replace($badWords, $replace, $text);
+    ?>
+    
+    <p>PARAGRAFO: </p>
+    <?php echo $text; ?><br>
+    <span>La Lunghezza del paragrafo è di <?php echo strlen($text); ?> caratteri.</span><br>
+    <span>Questa parola è stata censurata: <?php echo $badWords; ?></span>
+    <p>Paragrafo aggiornato: <?php echo $censuredText;?></p><br>
+    <span>Lunghezza del paragrafo aggiornato: <?php echo strlen($censuredText); ?> caratteri</span>
+</body>
+</html>
